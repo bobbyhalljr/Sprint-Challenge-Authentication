347405e6a39f48f399de0fb37861280af88d891d
Bobby Liberti <BobbyLiberti@Mes-iMac.local>
2019-10-18 13:59:36 -0400

    added more tests

--- jokes/jokes.spec.js
+++ jokes/jokes.spec.js
@@ -1,6 +1,40 @@
 const request = require('supertest');
 const server = require('../api/server');
-const jokes = require('../jokes/jokes-router');
+const authRouter = require('../auth/auth-router');
+
+let token;
+
+beforeAll((done) => {
+    request(server).post('/login')
+    .send({
+        username: authRouter.user,
+        password: authRouter.password
+    })
+    .end((err, response) => {
+        token = response.headers.token
+        done()
+    })
+})
+
+describe('GET /jokes', () => {
+    test('should require authorization', () => {
+        return request(server)
+        .get('/jokes')
+        .then(res => {
+            expect(res.status).toBe(404)
+        })
+    })
+
+    // test('should respond with JSON', () => {
+    //     return request(server)
+    //     .get('/jokes')
+    //     .set("Authorization", `${token}`)
+    //     .then(res => {
+    //         expect(res.status).toBe(200);
+    //         expect(res.type).toBe('application/json')
+    //     })
+    // })
+})
 
 describe('GET /jokes', () => {
     test('should return 200 http status code', async () => {
